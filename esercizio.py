a4a02008b84682b233ff70b878951ad1d5fe4c87
tombishop-83 <tombishop83@gmail.com>
2014-01-05 22:33:16 +0100

    Inserted some sample data

--- esercizio.py
+++ esercizio.py
@@ -1,3 +1,19 @@
+'''
+Sample data
+
+Mar 24 18:14:01 marco kernel: [10782.956702] usb 3-1.1.1: Manufacturer: Microsoft
+Mar 24 18:14:01 marco kernel: [10782.956873] usb 3-1.1.1: configuration #1 chosen from 1 choice
+Mar 24 18:14:01 marco kernel: [10782.973298] input: Microsoft Basic Optical Mouse as /devices/pci0000:00/0000:00:1d.1/usb3/3-1/3-1.1/3-1.1.1/3-1.1.1:1.0/input/input10
+Mar 24 18:14:01 marco kernel: [10782.974880] generic-usb 0003:045E:0084.0003: input,hidraw0: USB HID v1.10 Mouse [Microsoft Basic Optical Mouse] on usb-0000:00:1d.1-1.1.1/input0
+Mar 24 18:29:37 marco kernel: [11718.849291] usb 3-1.1.4: new full speed USB device using uhci_hcd and address 8
+Mar 24 18:29:37 marco kernel: [11718.951275] usb 3-1.1.4: not running at top speed; connect to a high speed hub
+Mar 24 18:29:37 marco kernel: [11718.979271] usb 3-1.1.4: New USB device found, idVendor=0fca, idProduct=8004
+Mar 24 18:29:37 marco kernel: [11718.979276] usb 3-1.1.4: New USB device strings: Mfr=1, Product=5, SerialNumber=3
+Mar 24 18:29:37 marco kernel: [11718.979281] usb 3-1.1.4: Product: RIM Composite Device
+
+'''
+
+
 def check_name(n_1, n_2):
  return n_1 == n_2
 '''
